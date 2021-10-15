f77b779d255d65eab750b9bf0a90497b5dc2e086
tithi23 <tithiratry@gmail.com>
2021-10-15 00:30:06 -0700

    simple firebase authentication application signout signin related problem

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCSg6F2sNtZdicH-GLaa-xD9zClD4Oznjw",
+    authDomain: "simple-firebase-authenti-5a177.firebaseapp.com",
+    projectId: "simple-firebase-authenti-5a177",
+    storageBucket: "simple-firebase-authenti-5a177.appspot.com",
+    messagingSenderId: "120915536805",
+    appId: "1:120915536805:web:526d301b74b14aad5a164c"
+};
+
+export default firebaseConfig;
